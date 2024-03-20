505509890d86ebfe8fe4dd7ec1f26635194b1a9c
ibadkhan213 <ibadk0693>
2024-03-20 23:30:17 +0500

    push my cal on github

---
+++ index.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+
+// simple calculator
+
+import inquirer from"inquirer";
+
+const answer = await inquirer.prompt([
+    {
+        message:"Enter first Number",
+        type:"number",
+        name:"number1"
+    },
+    {
+        message:"Enter second Number",
+        type:"number",
+        name:"number2"
+    },
+    {
+        message:"Select one of the operator to perform operation",
+        type:"list",
+        name:"operator",
+        choices:["Addition","Subtraction","Multiplication","Division"]
+        
+    },
+])
+
+if(answer.operator==="Addition"){
+    console.log(answer.number1 + answer.number2);
+} else if(answer.operator==="Subtraction"){
+    console.log(answer.number1 - answer.number2);
+}  else if(answer.operator==="Multiplication"){
+    console.log(answer.number1 * answer.number2);
+}  else if(answer.operator==="Division"){
+    console.log(answer.number1 / answer.number2);
+} else {
+    console.log("Please select valid option");
+}
